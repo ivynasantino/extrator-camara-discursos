b2a07c310f74c2b5411537474c21d608272c37fe
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-08-07 00:13:11 -0300

    extrai dados do pdf de previdência e adiciona estrutura inicial para o dataframe de discursos

---
+++ code/extract_previdencia.py
@@ -0,0 +1,20 @@
+#coding: utf-8
+import PyPDF2
+import sys
+
+doc_name = "Sessão da Câmara dos Deputados - [10-07-2019 09h00min]"
+obj = open("../files/nova_previdencia/" + doc_name + ".pdf", "rb")
+pdf_reader = PyPDF2.PdfFileReader(obj, strict=False)
+
+num_of_pages = pdf_reader.getNumPages()
+print('Número total de páginas do documento: %d' % num_of_pages)
+
+prev_file = open("../data/result.txt", 'w')
+for page in range(num_of_pages):
+    print('Página: %d' % page)
+    page_text = pdf_reader.getPage(page).extractText()
+    prev_file.write(page_text)
+    print()
+
+prev_file.close()
+obj.close()
