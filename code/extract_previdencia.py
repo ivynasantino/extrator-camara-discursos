ff259dcc594da7ad9b32a2309f8e10109286b4a7
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-08-11 22:27:10 -0300

    adiciona campo para extrair outro pdf

--- code/extract_previdencia.py
+++ code/extract_previdencia.py
@@ -2,14 +2,15 @@
 import PyPDF2
 import sys
 
-doc_name = "Sessão da Câmara dos Deputados - [10-07-2019 09h00min]"
+doc_name = "Sessão da Câmara dos Deputados - [10-07-2019 16h13min]"
 obj = open("../files/nova_previdencia/" + doc_name + ".pdf", "rb")
 pdf_reader = PyPDF2.PdfFileReader(obj, strict=False)
 
 num_of_pages = pdf_reader.getNumPages()
 print('Número total de páginas do documento: %d' % num_of_pages)
 
-prev_file = open("../data/result.txt", 'w')
+file_name = "result_2.txt"
+prev_file = open("../data/", 'w')
 for page in range(num_of_pages):
     print('Página: %d' % page)
     page_text = pdf_reader.getPage(page).extractText()
