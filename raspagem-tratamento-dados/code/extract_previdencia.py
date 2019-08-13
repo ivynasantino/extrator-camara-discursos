d85212380e934ce1f936c67ccb25201725aeab1d
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-08-13 17:14:57 -0300

    extrai todos os textos dos pdfs referentes a reforma da previdência

--- raspagem-tratamento-dados/code/extract_previdencia.py
+++ raspagem-tratamento-dados/code/extract_previdencia.py
@@ -2,7 +2,7 @@
 import PyPDF2
 import sys
 
-doc_name = "Sessão da Câmara dos Deputados - [10-07-2019 16h13min]"
+doc_name = "Sessão da Câmara dos Deputados - [06-08-2019 21h39min]"
 obj = open("../files/nova_previdencia/" + doc_name + ".pdf", "rb")
 pdf_reader = PyPDF2.PdfFileReader(obj, strict=False)
 
@@ -10,12 +10,11 @@ num_of_pages = pdf_reader.getNumPages()
 print('Número total de páginas do documento: %d' % num_of_pages)
 
 file_name = "result_2.txt"
-prev_file = open("../data/", 'w')
+prev_file = open("../data/" + file_name, 'w')
 for page in range(num_of_pages):
     print('Página: %d' % page)
     page_text = pdf_reader.getPage(page).extractText()
     prev_file.write(page_text)
-    print()
-
+    
 prev_file.close()
 obj.close()
