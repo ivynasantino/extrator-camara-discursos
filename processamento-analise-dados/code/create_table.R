a3602b6f8fb458e78fd0bce29f333705122c954c
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-08-13 21:52:35 -0300

    adiciona csv referente a sessao 06-08-2019 21h

--- processamento-analise-dados/code/create_table.R
+++ processamento-analise-dados/code/create_table.R
@@ -2,13 +2,13 @@ library(readr)
 library(magrittr)
 library(tidyverse)
 
-export_path <- "previdencia_10-07-16h"
-name_txt <- "previdencia_10-7-16h"
+export_path <- "previdencia_06-08-21h"
+name_txt <- "previdencia_06-08-21h"
 
-previdencia <- read.csv("../raspagem-tratamento-dados/data/previdencia_10-7-16h.txt", sep = ";")
+previdencia <- read.csv(paste0("../raspagem-tratamento-dados/data/", name_txt, ".txt"), sep = "|")
 
 previdencia <- previdencia %>% 
-  mutate(data = "10/07/2019",
-         id_sessao = 191)
+  mutate(data = "06/08/2019",
+         id_sessao = 207)
 
 write_csv(previdencia, paste0("data/", export_path, ".csv"))
