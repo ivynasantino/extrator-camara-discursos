668de64e09ca597aa96b5326942fa28b38ffb7b3
Ivyna Santino <ivyna.alves@ccc.ufcg.edu.br>
2019-09-10 21:15:49 -0300

    refatora script de extração de texto em pdfs

--- raspagem-tratamento-dados/code/extract_previdencia.py
+++ raspagem-tratamento-dados/code/extract_previdencia.py
@@ -1,23 +1,50 @@
 #coding: utf-8
 #!/usr/bin/python3
 
-import PyPDF2
-import sys
-import re
+__author__ = "Ivyna Santino"
+__maintainer__ = "Ivyna Santino"
+__email__ = "ivyna.alves@ccc.ufcg.edu.br"
+__version__ = "1.0.0"
 
-doc_name = "Câmara dos Deputados - Reunião de Comissão - CCJC - [16-04-2019 10h34min]"
-obj = open("../files/nova_previdencia/ccj/" + doc_name + ".pdf", "rb")
-pdf_reader = PyPDF2.PdfFileReader(obj, strict=False)
+def leitura_arquivo(caminho):
+	obj = open(caminho, 'rb')
+	pdf_reader = PyPDF2.PdfFileReader(obj, strict=False)
+	return pdf_reader
 
-num_of_pages = pdf_reader.getNumPages()
-print('Número total de páginas do documento: %d' % num_of_pages)
+def escreve_arquivo(caminho, texto_pdf):
+	obj = open(caminho, 'w')
+	obj.write(texto_pdf)
 
-file_name = "ccjc_reuniao-16-04-2019_10h.txt"
-prev_file = open("../data/extract-texts-pdf/" + file_name, 'w')
-for page in range(num_of_pages):
-    print('Página: %d' % page)
-    page_text = pdf_reader.getPage(page).extractText()
-    prev_file.write(page_text)
-    
-prev_file.close()
-obj.close()
+def dados_pdf(obj_pdf, nome_arquivo):
+	numero_paginas = pdf_reader.getNumPages()
+	print('Dados do documento')
+	print('====================')
+	print('%s tem %d páginas' % (nome_arquivo))
+	print()
+	return(numero_paginas)
+
+def extrai_texto_pdf(obj_pdf, numero_paginas):
+	print('Inicia extração dos textos')
+	print('==========================')
+	print()
+	for pagina in range(numero_paginas):
+		pagina_texto = obj_pdf.getPage(pagina).extractText()
+		print('O texto da página %d foi extraído' % (pagina))
+	return pagina_texto
+
+def main():
+	pasta = ""
+	nome_arquivo = ""
+	caminho = pasta + nome_arquivo
+
+	obj_pdf = leitura_arquivo(caminho)
+	dados_documento = dados_pdf(obj_pdf, nome_arquivo)
+
+	texto_pdf = extrai_texto_pdf(obj_pdf, dados_documento)
+
+	pasta_final = ""
+	caminho_final = pasta_final + nome_arquivo
+	arquivo_final = escreve_arquivo(, texto_pdf)
+
+	arquivo_final.close()
+	obj_pdf.close()
